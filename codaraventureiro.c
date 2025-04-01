e8c8d15d0527ccddbf8e00fa7815f097d2d3fb86
tishirley <vipconvitesjf@gmail.com>
2025-04-01 17:15:43 +0000

    unsigned

---
+++ codaraventureiro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+float nota1, nota2, nota3;
+float media;
+
+printf("*** Programa de Cálculo de Média ***\n");
+
+printf("Digite a sua primeira nota: \n");
+scanf("%f", &nota1);
+
+printf("Digite a sua segunda nota: \n");
+scanf("%f", &nota2);
+
+printf("Digite a sua terceira nota: \n");
+scanf("%f", &nota3);
+
+media = (float)
+(nota1 + nota2 + nota3) / 3;
+
+printf("A média é: %.1f", media);
+
+return 0;
+   
+}
